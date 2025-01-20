c2677d9291cd01ca83ca6d385fba0b2b7614bb1b
sharath-c-s <saisarath.chandra1111@gmail.com>
2025-01-21 00:57:58 +0530

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Countdown from "./components/Countdown";
 import Wish from "./components/Wish";
 
